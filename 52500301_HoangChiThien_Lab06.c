89ebd12436f3dcffbe68f6ebb8e7b21a27d997e6
Kasuo-3107 <otaku251107@gmail.com>
2026-04-23 18:04:00 +0000

    Last update Lab06

--- 52500301_HoangChiThien_Lab06.c
+++ 52500301_HoangChiThien_Lab06.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int checkPrime(int n,int i){
     if (n < 2)
@@ -23,20 +24,84 @@ int computeFactorial(int n){
 }
 
 int computeTwoHeadn(int n){
-    
-    return 2 ** n;
+    if (n == 0) {
+        return 1;
+    }
+    return 2 * computeTwoHeadn(n - 1);
 }
+
+int computeXHeadn(int x, int n){
+    if (n == 0) {
+        return 1;
+    }
+    return x * computeXHeadn(x, n - 1);
+}
+
+int returnNumberOfDigitsOfPositiveInteger(int n){
+    if (n == 0) {
+        return 0;
+    }
+    return 1 + returnNumberOfDigitsOfPositiveInteger(n / 10);
+}
+
+int sumTwoIPlusOne(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return (2 * n + 1) + sumTwoIPlusOne(n - 1);
+}
+
+double sumIDivideTwo(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return (double)n / 2 + sumIDivideTwo(n - 1);
+}
+
+int sumFactorial(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return computeFactorial(n) + sumFactorial(n - 1);
+}
+
+double sumSqrtI(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return sqrt(n) + sumSqrtI(n - 1);
+}
+
+long long productFactorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return (long long)computeFactorial(n) * productFactorial(n - 1);
+}
+
 
 int main()  {   
     int n;
+    printf("Add n: ");
     scanf("%d",&n);
     if (checkPrime(n, 2) == 1)
     {
-        printf("Prime");
+        printf("CheckPrime: Prime\n");
     }   else
     {
-        printf("NOT Prime");
+        printf("CheckPrime: NOT Prime\n");
     }
-
+    printf("ComputeFactorial: %d\n", computeFactorial(n));
+    printf("ComputeTwoHeadn: %d\n", computeTwoHeadn(n));
+    int x;
+    printf("Add x: ");
+    scanf("%d",&x);
+    printf("ComputeXHeadn: %d\n", computeXHeadn(x, n));
+    printf("ReturnNumberOfDigitsOfPositiveInteger: %d\n", returnNumberOfDigitsOfPositiveInteger(n));
+    printf("SumTwoIPlusOne: %d\n", sumTwoIPlusOne(n));
+    printf("SumIDivideTwo: %.2lf\n", sumIDivideTwo(n));
+    printf("SumFactorial: %d\n", sumFactorial(n));
+    printf("SumSqrtI: %.2lf\n", sumSqrtI(n));
+    printf("ProductFactorial: %lld\n", productFactorial(n));
     return 0;
 }
